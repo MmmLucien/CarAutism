a433f2be51269fa469e1ec5ef5097be491de0f3d
MmmLucien <137907547+MmmLucien@users.noreply.github.com>
2026-07-01 14:09:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      selfDestroying: true,
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'icons/*.png'],
       manifest: {
         name: 'CarAutism',
